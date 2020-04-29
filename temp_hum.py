90e6d1e18a5c6fb049432f7c4249865a0bcddb4e
lucabar <lucabarbera@posteo.de>
2020-04-29 16:36:59 +0200

    modified:   temp_hum.py

--- temp_hum.py
+++ temp_hum.py
@@ -11,40 +11,42 @@
 
 import numpy
 from tango import AttrWriteType, DevState
-from tango import InfoIt, LogIt, DebugIt
+from tango import LogIt, DebugIt
 from tango.server import Device, attribute, command
 import AM2315 as am_driver
 
 class TempHum(Device):
     
-    temperature = attribute(access=AttrWriteType.READ,
-                            fget='get_temperature')
+    temperature = attribute(name='Temperature',access=AttrWriteType.READ,
+                            dtype=float,fget='get_temperature',format='.2f',
+                            min_value=-273.15,doc='the measured temperature',
+                            )
     
-    humidity = attribute(access=AttrWriteType.READ,
-                        fget='get_humidity')
+    humidity = attribute(name='Humidity',access=AttrWriteType.READ
+                         ,dtype=float,fget='get_humidity',doc='the measured humidity',
+                         )
     
     def init_device(self):
-        Device.init_device(self)
-        self.am2315 = am_driver.AM2315()
-        self.set_state(DevState.ON)
-        self.temp = 0
-        self.humid = 0
-        
-     
+        try:
+            Device.init_device(self)
+            self.am2315 = am_driver.AM2315()
+            self.set_state(DevState.ON)
+            self.temp = 0
+            self.humid = 0
+        except:
+            return('There was no AM2315-Device found.')
+             
     @command(polling_period=500)
     def get_data(self):
-        #print('one')
         #_read_data measures both humidity and temperature
         self.am2315._read_data()
         self.temp = self.am2315.temperature
         self.humid = self.am2315.humidity
-        
-        
-    @LogIt(show_ret=True)   
+        return self.temp
+                   
     def get_temperature(self):
         return self.temp
-    
-    
+        
     def get_humidity(self):
         return self.humid
         
