7b31869cebcde38357da274de652d931f41bf6d8
lucabar <lucabarbera@posteo.de>
2020-04-30 12:33:43 +0200

    modified:   temp_hum.py

--- temp_hum.py
+++ temp_hum.py
@@ -10,8 +10,8 @@
 """
 
 import numpy
-from tango import AttrWriteType, DevState
-from tango import LogIt, DebugIt
+from tango import AttrWriteType, DevState, ErrorIt, FatalIt
+from tango import LogIt, DebugIt, InfoIt, WarnIt
 from tango.server import Device, attribute, command
 import AM2315 as am_driver
 
@@ -19,12 +19,11 @@ class TempHum(Device):
     
     temperature = attribute(name='Temperature',access=AttrWriteType.READ,
                             dtype=float,fget='get_temperature',format='.2f',
-                            min_value=-273.15,doc='the measured temperature',
-                            )
+                            min_value=-273.15,doc='the measured temperature')
     
     humidity = attribute(name='Humidity',access=AttrWriteType.READ
-                         ,dtype=float,fget='get_humidity',doc='the measured humidity',
-                         )
+                         ,dtype=float,fget='get_humidity',format='.2f',
+                         doc='the measured humidity')
     
     def init_device(self):
         try:
@@ -33,23 +32,26 @@ class TempHum(Device):
             self.set_state(DevState.ON)
             self.temp = 0
             self.humid = 0
+            
         except:
             return('There was no AM2315-Device found.')
-             
+        
+    @WarnIt()            
     @command(polling_period=500)
     def get_data(self):
         #_read_data measures both humidity and temperature
         self.am2315._read_data()
         self.temp = self.am2315.temperature
         self.humid = self.am2315.humidity
-        return self.temp
-                   
+        
+    @DebugIt(show_ret=True)               
     def get_temperature(self):
         return self.temp
-        
+    
+    @DebugIt(show_ret=True)    
     def get_humidity(self):
-        return self.humid
-        
+        return self.humid   
+
     def read_state(self):
         return self.state()
 
