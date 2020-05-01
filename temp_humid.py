8078b0be79ed59005118fef25e4dace5cf607753
lucabar <lucabarbera@posteo.de>
2020-05-01 11:06:58 +0200

    renamed:    temp_hum.py -> temp_humid.py

--- temp_humid.py
+++ temp_humid.py
@@ -15,7 +15,7 @@ from tango import LogIt, DebugIt, InfoIt, WarnIt
 from tango.server import Device, attribute, command
 import AM2315 as am_driver
 
-class TempHum(Device):
+class TempHumid(Device):
     
     temperature = attribute(name='Temperature',access=AttrWriteType.READ,
                             dtype=float,fget='get_temperature',format='.2f',
@@ -26,35 +26,42 @@ class TempHum(Device):
                          doc='the measured humidity')
     
     def init_device(self):
+        self.info_stream('Trying to connect device to server.')
         try:
             Device.init_device(self)
             self.am2315 = am_driver.AM2315()
             self.set_state(DevState.ON)
             self.temp = 0
             self.humid = 0
-            self.info_stream("conenction established")
+            self.info_stream("Connection established.")
             
         except:
-            return('There was no AM2315-Device found.')
+            self.error_stream('Connection could not be established.')
         
     @DebugIt()            
-    @command(polling_period=500)
+    @command()
     def get_data(self):
         #_read_data measures both humidity and temperature
         self.am2315._read_data()
         self.temp = self.am2315.temperature
         self.humid = self.am2315.humidity
         
-    @InfoIt(show_ret=True)               
+    @DebugIt(show_ret=True)               
     def get_temperature(self):
         return self.temp
     
-    @InfoIt(show_ret=True)    
+    @DebugIt(show_ret=True)    
     def get_humidity(self):
         return self.humid   
-
+    
+    @ErrorIt()
+    @command()
+    def error_func(self):
+        print('You have made an error.')
+        return None
+    
     def read_state(self):
         return self.state()
 
 if __name__ == "__main__":
-    TempHum.run_server()
+    TempHumid.run_server()
