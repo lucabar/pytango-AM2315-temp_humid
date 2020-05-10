33e21dcd05a0b85e92735d944ff00e451dc4c165
lucabar <lucabarbera@posteo.de>
2020-05-10 12:01:55 +0200

    modified:   temp_humid.py

--- temp_humid.py
+++ temp_humid.py
@@ -9,16 +9,19 @@
 
 """
 
-
+import busio, board
 from tango import AttrWriteType, DevState, ErrorIt, DebugIt
 from tango.server import Device, attribute, command
 import adafruit_am2320 as am_driver
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
 
 class TempHumid(Device):
     
     temperature = attribute(name='Temperature',access=AttrWriteType.READ,
                             dtype=float,fget='get_temperature',format='.2f',
-                            min_value=-273.15,doc='the measured temperature',unit='°C')
+                            min_value=-273.15,doc='the measured temperature',unit='C')
     
     humidity = attribute(name='Humidity',access=AttrWriteType.READ
                          ,dtype=float,fget='get_humidity',format='.2f',
@@ -28,28 +31,31 @@ class TempHumid(Device):
         self.info_stream('Trying to connect device to server.')
         try:
             Device.init_device(self)
-            self.am2315 = am_driver.AM2320()
+            self.am2315 = am_driver.AM2320(i2c)
             self.set_state(DevState.ON)
             self.temp = 0
             self.humid = 0
             self.info_stream("Connection established.")
-            
         except:
             self.error_stream('Connection could not be established.')
         
+    
     @DebugIt()            
     @command()
     def get_data(self):
-        self.temp = self.am2315.temperature
-        self.humid = self.am2315.humidity
-        
+        try:
+            self.temp = self.am2315.temperature
+            self.humid = self.am2315.relative_humidity
+        except:
+            self.error_stream('Could not measure!')
+
     @DebugIt(show_ret=True)               
     def get_temperature(self):
         return self.temp
     
     @DebugIt(show_ret=True)    
     def get_humidity(self):
-        return self.humid   
+        return self.humid
     
     @ErrorIt()
     @command()
@@ -62,3 +68,4 @@ class TempHumid(Device):
 
 if __name__ == "__main__":
     TempHumid.run_server()
+
