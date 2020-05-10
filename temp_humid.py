2bb30f397d83ae571a24403411550981afc968e9
lucabar <lucabarbera@posteo.de>
2020-05-10 12:04:28 +0200

    modified:   temp_humid.py

--- temp_humid.py
+++ temp_humid.py
@@ -4,58 +4,53 @@
     temperature and humidity measurement Tango Device Server
     
     Created on Wed Apr 22 15:19:58 2020
-
     @author: mbi
-
 """
-
-import busio, board
-from tango import AttrWriteType, DevState, ErrorIt, DebugIt
+import numpy
+from tango import AttrWriteType, DevState, ErrorIt, FatalIt
+from tango import LogIt, DebugIt, InfoIt, WarnIt
 from tango.server import Device, attribute, command
-import adafruit_am2320 as am_driver
-
-i2c = busio.I2C(board.SCL, board.SDA)
-
-
+import AM2315 as am_driver
 class TempHumid(Device):
-    
+
     temperature = attribute(name='Temperature',access=AttrWriteType.READ,
                             dtype=float,fget='get_temperature',format='.2f',
-                            min_value=-273.15,doc='the measured temperature',unit='C')
-    
+                            min_value=-273.15,doc='the measured temperature')
+                            min_value=-273.15,doc='the measured temperature',unit='°C')
+
     humidity = attribute(name='Humidity',access=AttrWriteType.READ
                          ,dtype=float,fget='get_humidity',format='.2f',
+                         doc='the measured humidity')
                          doc='the measured humidity',unit='%')
-    
+
     def init_device(self):
         self.info_stream('Trying to connect device to server.')
         try:
             Device.init_device(self)
-            self.am2315 = am_driver.AM2320(i2c)
+            self.am2315 = am_driver.AM2315()
             self.set_state(DevState.ON)
             self.temp = 0
             self.humid = 0
             self.info_stream("Connection established.")
+            
         except:
             self.error_stream('Connection could not be established.')
         
-    
     @DebugIt()            
     @command()
     def get_data(self):
-        try:
-            self.temp = self.am2315.temperature
-            self.humid = self.am2315.relative_humidity
-        except:
-            self.error_stream('Could not measure!')
-
+        #_read_data measures both humidity and temperature
+        self.am2315._read_data()
+        self.temp = self.am2315.temperature
+        self.humid = self.am2315.humidity
+        
     @DebugIt(show_ret=True)               
     def get_temperature(self):
         return self.temp
     
     @DebugIt(show_ret=True)    
     def get_humidity(self):
-        return self.humid
+        return self.humid   
     
     @ErrorIt()
     @command()
@@ -65,7 +60,5 @@ class TempHumid(Device):
     
     def read_state(self):
         return self.state()
-
 if __name__ == "__main__":
     TempHumid.run_server()
-
